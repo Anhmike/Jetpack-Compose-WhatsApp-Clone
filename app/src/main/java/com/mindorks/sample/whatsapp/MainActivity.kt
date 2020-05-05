28b2bbecf4c99f98df1cbb3830209a20fad4cb86
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-05-05 13:23:11 +0530

    Project Update

--- app/src/main/java/com/mindorks/sample/whatsapp/MainActivity.kt
+++ app/src/main/java/com/mindorks/sample/whatsapp/MainActivity.kt
@@ -1,33 +1,31 @@
 package com.mindorks.sample.whatsapp
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.Composable
+import androidx.activity.ComponentActivity
+import androidx.compose.Providers
 import androidx.ui.core.setContent
-import androidx.ui.foundation.Text
-import androidx.ui.material.MaterialTheme
-import androidx.ui.tooling.preview.Preview
+import com.mindorks.sample.whatsapp.ambients.fakerRepository
+import com.mindorks.sample.whatsapp.data.repository.FakerRepository
+import com.mindorks.sample.whatsapp.screen.App
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
+    private val scope = MainScope()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            MaterialTheme {
-                Greeting("Android")
+            Providers(fakerRepository provides FakerRepository()) {
+                App(scope)
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
     }
 }
